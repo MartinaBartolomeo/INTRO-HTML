f533ef3b328a7e9bfd0dc622195cd9f3776c4d9b
MartinaBartolomeo <martinabartolomeo01@gmail.com>
2022-11-29 19:29:57 -0300

    Proyecto hasta M04C19

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static("./public"));
 
 app.listen(3000, () =>{
-    console.log("servidor corriendo en http://localhost/3000")
+    console.log("servidor corriendo en http://localhost:3000")
 });
 
 app.get('/', (req, res) =>{
